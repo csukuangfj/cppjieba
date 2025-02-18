6e5d9c9a8fe0145f86a7d1a872b7f5b085647bba
Fangjun Kuang <csukuangfj@gmail.com>
2025-02-18 12:30:07 +0800

    update user dict

--- dict/generate_user_dict.py
+++ dict/generate_user_dict.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-from pypinyin import phrases_dict
+from pypinyin import phrases_dict, load_phrases_dict
+
+user_dict = {
+    "还田": [["huan2"], ["tian2"]],
+    "行长": [["hang2"], ["zhang3"]],
+    "银行行长": [["yin2"], ["hang2"], ["hang2"], ["zhang3"]],
+}
+
+load_phrases_dict(user_dict)
+
+phrases_dict.phrases_dict.update(**user_dict)
 
 
 def main():
